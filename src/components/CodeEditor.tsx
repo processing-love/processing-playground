9975c83d890cabbda52559411bc7ca31a8cb61e8
peter <1611641037@qq.com>
2023-03-10 00:59:10 +0800

    引用组件

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,31 +1,16 @@
-import React, {useEffect, useRef} from "react";
-import {EditorView} from "@codemirror/view";
-import {EditorState} from "@codemirror/state";
 import Code from "../model/response/Code";
+import ReactCodeMirror from "@uiw/react-codemirror";
 
 interface ChildProps {
     code: Code
 }
 
 const CodeEditor: React.FC<ChildProps> = (props) => {
-    const edContainer = useRef<any>();
     const {code} = props;
-    useEffect(() => {
-        const state = EditorState.create({
-            doc: code.content,
-        });
-        const view = new EditorView(
-            {
-                parent: edContainer.current,
-                state,
-            }
-        );
-        return () => {
-            view.destroy();
-        }
-    }, []);
-    return <div ref={edContainer}>
+    return <ReactCodeMirror
+        height="200px"
+        value={code.content}>
 
-    </div>
+    </ReactCodeMirror>
 }
 export default CodeEditor;
